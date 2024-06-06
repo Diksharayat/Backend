dd44cb938329f1317131593ba91555f79147686e
Diksharayat <rayatdiksha787@gmail.com>
2024-06-06 16:27:53 +0530

    front link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app=express();
 
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'https://frontend-pink-eight.vercel.app/', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
