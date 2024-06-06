55c1ab7b503b1004d994012e60d600e13d481f14
Diksharayat <rayatdiksha787@gmail.com>
2024-06-06 16:23:33 +0530

    cors1

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-const express = require('express');
+const express =require('express');
 const cart_route = require('./routes/cartRoute');
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser =require("body-parser");
+const cors= require("cors");
 require("./config/dbConnect");
-const app = express();
+const app=express();
 
-// Set CORS headers using custom middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-pink-eight.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+//middlewares
+app.use(cors())
 
 // Middleware
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
   console.log(req.useragent)
   res.send('Hey this is my API running 🥳')
-});
+})
 
+
+app.use(express.json())
 //routes
-app.use("/api", cart_route);
 
-// Error handlers
+app.use("/api",cart_route);
 
-// Listen to the server.
-const port = process.env.PORT || 10000;
-app.listen(port, console.log(`SERVER IS UP AND RUNNING ON PORT ${port}`));
+//cart route
+
+
+//Error handlers
+
+
+//listen to the server.
+const port = process.env.PORT||10000;
+app.listen(port,console.log(`SERVER IS UP AND RUNNING ON PORT ${port}`));
