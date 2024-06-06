9ef706edd319be1e209b809876d88e700675d9bc
Diksharayat <rayatdiksha787@gmail.com>
2024-06-06 15:55:56 +0530

    cors error

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ require("./config/dbConnect");
 const app=express();
 
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-pink-eight.vercel.app/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 // Middleware
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
