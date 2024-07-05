207230fa1dd79cbad9e5fa49c935615b0fcf2c33
Diksharayat <rayatdiksha787@gmail.com>
2024-07-05 11:12:31 +0530

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 var dotenv = require('dotenv');
 const User = require('../models/User');
 dotenv.config()
